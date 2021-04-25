fb5412bc95e3aa478f572f3c475c970238968f93
Hidetake Iwata <int128@gmail.com>
2021-04-25 12:30:44 +0900

    Exclude lib/ (#5)

--- hello-world/lib/tests/message.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const message_1 = require("../src/message");
-test('message is expected one', () => {
-    const message = message_1.getMessage('foo');
-    expect(message).toBe('Hello World foo');
-});

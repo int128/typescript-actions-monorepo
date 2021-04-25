d97ce8e285c9c9387052a144a11f5d6d936a282d
Hidetake Iwata <int128@gmail.com>
2021-04-25 12:26:34 +0900

    Exclude lib/

--- hello-world/lib/src/message.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getMessage = void 0;
-const getMessage = (name) => `Hello World ${name}`;
-exports.getMessage = getMessage;

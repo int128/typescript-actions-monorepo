91364678da4a8b5480007dc56ca3215425ecb262
Hidetake Iwata <int128@gmail.com>
2021-04-25 12:47:51 +0900

    Fix update-generated-files

--- .github/actions/update-generated-files/run.sh
+++ .github/actions/update-generated-files/run.sh
@@ -2,7 +2,8 @@
 set -o pipefail
 set -eux
 
-if git diff --exit-code; then
+status="$(git status --porcelain)"
+if [ -z "$status" ]; then
   exit 0
 fi
 
@@ -14,5 +15,6 @@ fi
 git config user.name 'github-actions[bot]'
 git config user.email '41898282+github-actions[bot]@users.noreply.github.com'
 git add .
+git status
 git commit -m update-generated-files
 git push origin "${GITHUB_HEAD_REF}"

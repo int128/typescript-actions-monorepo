6808bc53d6881fa9cb7ccec26d3f353f402ca52d
Hidetake Iwata <int128@gmail.com>
2021-08-12 09:38:12 +0900

    Fix lint errors

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -2,13 +2,9 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  try {
-    await run({
-      name: core.getInput('name', { required: true }),
-    })
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  await run({
+    name: core.getInput('name', { required: true }),
+  })
 }
 
-main()
+main().catch((error) => core.setFailed(error))
--- hello-world/src/run.ts
+++ hello-world/src/run.ts
@@ -4,6 +4,7 @@ interface Inputs {
   name: string
 }
 
+// eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (inputs: Inputs): Promise<void> => {
   core.info(`my name is ${inputs.name}`)
 }
